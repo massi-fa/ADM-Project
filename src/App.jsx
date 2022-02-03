047b7e6b33c1511e169b27389529458d96ec5026
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2022-02-03 11:21:03 +0100

    Merge branch 'main' of https://github.com/massi-fa/ADM-Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 /* eslint-disable */
 import React from 'react';
-import { initializeApp } from "firebase/app"
-import { getFirestore,collection, query, where,getDocs,then  } from "firebase/firestore"
-import { get  } from 'firebase/database';
-import firebase from "firebase/compat";
-const firebaseConfig = {
-  apiKey: "AIzaSyB5U9rNuMCcTH4Fz_Y49Jg5YZrLdbX2wh4",
-  authDomain: "adm-practice-test.firebaseapp.com",
-  databaseURL: "https://adm-practice-test-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "adm-practice-test",
-  storageBucket: "adm-practice-test.appspot.com",
-  messagingSenderId: "580166880322",
-  appId: "1:580166880322:web:3b8e56eb5c81fd0227b1db",
-  measurementId: "G-YZV05N2KPM"
-};
+import db from './firebase';
 
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-console.log(firebaseApp);
-const db = firebase.firestore();
 db.collection("Grotte").where("COMUNE", "==", "ALGHERO")
     .get()
     .then((querySnapshot) => {
@@ -32,12 +15,11 @@ db.collection("Grotte").where("COMUNE", "==", "ALGHERO")
     });
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ciao</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>ciao</h1>
+  </div>
+);
+
 
 export default App;
